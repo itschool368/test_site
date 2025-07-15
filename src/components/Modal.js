365bae73c65961a1a77bc708179b4369f962baa2
itschool368 <itdigital.age@gmail.com>
2025-07-15 17:42:00 +0300

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,7 +18,7 @@ const Modal = ({ isOpen, onClose }) => {
     };
 
     try {
-      const res = await fetch('https://13.53.200.62/contact', {
+      const res = await fetch('https://13.53.200.62:3001/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
