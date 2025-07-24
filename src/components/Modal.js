63c40a3cead7f05a629d4f862050d29ac2f28281
itschool368 <itdigital.age@gmail.com>
2025-07-24 14:00:11 +0300

    11q

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,7 +18,7 @@ const Modal = ({ isOpen, onClose }) => {
     };
 
     try {
-      const res = await fetch('https://flowerslife.duckdns.org/contact', {
+      const res = await fetch('https://digital-age.duckdns.org/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
